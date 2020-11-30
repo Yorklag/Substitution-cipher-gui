eff833bfba94477dea1529bbf5130af749eb6cec
Yorklag <74254148+Yorklag@users.noreply.github.com>
2020-11-30 03:15:51 -0600

    Initial commit

---
+++ Substitution cipher gui/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Substitution_cipher_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.RichTextBox();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.encodeButton = new System.Windows.Forms.Button();
+            this.decodeButton = new System.Windows.Forms.Button();
+            this.cypherNumberBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.cypherNumberBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(776, 142);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.Text = "Input Text Goes Here";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(12, 296);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(776, 142);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.Text = "Output Text Goes Here";
+            // 
+            // encodeButton
+            // 
+            this.encodeButton.Location = new System.Drawing.Point(236, 215);
+            this.encodeButton.Name = "encodeButton";
+            this.encodeButton.Size = new System.Drawing.Size(75, 23);
+            this.encodeButton.TabIndex = 2;
+            this.encodeButton.Text = "Encode";
+            this.encodeButton.UseVisualStyleBackColor = true;
+            this.encodeButton.Click += new System.EventHandler(this.encodeButton_Click);
+            // 
+            // decodeButton
+            // 
+            this.decodeButton.Location = new System.Drawing.Point(443, 215);
+            this.decodeButton.Name = "decodeButton";
+            this.decodeButton.Size = new System.Drawing.Size(75, 23);
+            this.decodeButton.TabIndex = 3;
+            this.decodeButton.Text = "Decode";
+            this.decodeButton.UseVisualStyleBackColor = true;
+            this.decodeButton.Click += new System.EventHandler(this.decodeButton_Click);
+            // 
+            // cypherNumberBox
+            // 
+            this.cypherNumberBox.Location = new System.Drawing.Point(317, 215);
+            this.cypherNumberBox.Name = "cypherNumberBox";
+            this.cypherNumberBox.Size = new System.Drawing.Size(120, 20);
+            this.cypherNumberBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cypherNumberBox);
+            this.Controls.Add(this.decodeButton);
+            this.Controls.Add(this.encodeButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Shift Encoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cypherNumberBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox inputTextBox;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Button encodeButton;
+        private System.Windows.Forms.Button decodeButton;
+        private System.Windows.Forms.NumericUpDown cypherNumberBox;
+    }
+}
+
---
+++ Substitution cipher gui/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Substitution_cipher_gui
+{
+    public partial class Form1 : Form
+    {
+        static string alphabet = " abcdefghijklmnopqrstuvwxyz0123456789";
+        char[] alphabetCharArray = alphabet.ToCharArray();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string encodeString(bool encode, string input)
+        {
+            if (String.IsNullOrEmpty(input))
+            {
+                return "";
+            }
+            else
+            {
+                input = input.ToLower();
+                int shift = (int)cypherNumberBox.Value;
+                string output = "";
+                char[] inputArray = input.ToCharArray();
+                bool isValid = true;
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    int index = Array.IndexOf(alphabetCharArray, inputArray[i]);
+                    if (index == -1)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                    if (encode)
+                    {
+                        index = index + shift;
+                        while (index >= alphabetCharArray.Length)
+                        {
+                            index = index - alphabetCharArray.Length;
+                        }
+                    }
+                    else
+                    {
+                        index = index - shift;
+                        while (index < 0)
+                        {
+                            index = index + alphabetCharArray.Length;
+                        }
+                    }
+                    output += alphabetCharArray[index];
+                }
+                if (isValid)
+                {
+                    return output;
+                }
+                else
+                {
+                    return "unable to encode/decode string";
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void encodeButton_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = encodeString(true, inputTextBox.Text);
+        }
+
+        private void decodeButton_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = encodeString(false, inputTextBox.Text);
+        }
+    }
+}
